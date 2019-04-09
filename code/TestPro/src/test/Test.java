5b6ff098438e24fdd08d0f514970cb476b8600d2
jinhaiZeng <1270417137@qq.com>
2019-04-09 14:17:54 +0800

    复习java基本语法，并修改了相应笔记

---
+++ code/TestPro/src/test/Test.java
@@ -0,0 +1,20 @@
+package test;
+import testclass.Cat;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cat one = new Cat(50);
+		System.out.println(one.getAge());
+//		String a = "hello world!";
+//		System.out.println(a);
+		
+		int[] temp = new int[10];
+		System.out.println(temp);
+		for(int n:temp) {
+			System.out.println(temp[n]);
+		}
+	}
+
+}
---
+++ code/TestPro/src/testclass/Cat.java
@@ -0,0 +1,15 @@
+package testclass;
+
+public class Cat {
+	final int age;
+	
+	public Cat(int age) {
+		this.age = age;
+	}
+
+	
+	public int getAge() {
+		return age;
+	}
+	
+}
